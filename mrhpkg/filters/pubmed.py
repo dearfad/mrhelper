3f07ca4c6001ec9ca18eb8f884098a5a7abc8a11
dearfad <dearfad@sina.com>
2018-08-18 07:18:44 +0800

    update

--- mrhpkg/filters/pubmed.py
+++ mrhpkg/filters/pubmed.py
@@ -200,7 +200,8 @@ def opendir(datafile_path):
 
 if __name__ == '__main__':
     # Datafile Path
-    datafile_path = '../../data/'
+    datafile_path = 'pubmed.txt'
     # Read Data
     data = getdata(datafile_path)
+    print(len(data))
 # endregion
